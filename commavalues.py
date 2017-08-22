bd03ffc37ff4603f3291f9ef6050949c020b44b7
ramki123456 <ramki0653@gmail.com>
2017-08-22 21:15:24 +0530

    Update commavalues.py

--- commavalues.py
+++ commavalues.py
@@ -1,16 +1,19 @@
 import csv
 import sqlite3
+import time
+import datetime
+ts = time.time()
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 conn = sqlite3.connect('/home/brahma/Documents/EmpManagement/sqlite3.db')
 cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS aug19")
-cur.execute("CREATE TABLE Aug19 (ID INTEGER, PRODUCTNAME TEXT, TYPE TEXT, MODEL TEXT, LOCATION TEXT);")
+cur.execute("CREATE TABLE Aug19 (ID INTEGER, PRODUCTNAME TEXT, TYPE TEXT, MODEL TEXT, LOCATION TEXT, DATE TEXT);")
 with open('/home/brahma/Documents/personal/excelfiles/Data.csv', 'r') as f:
     r = csv.reader(f, delimiter=',')
     next(r)
     for row in r:
     	data = row
     	cur.execute(
-        '''INSERT INTO Aug19(ID, PRODUCTNAME, TYPE, MODEL,
-       LOCATION) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');'''.format(
-            data[0], data[1], data[2], data[3], data[4]))
+        '''INSERT INTO Aug19(ID, PRODUCTNAME, TYPE, MODEL, LOCATION, DATE) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}');'''.format(
+            data[0], data[1], data[2], data[3], data[4], time.strftime("%d/%m/%Y")))
     conn.commit()
