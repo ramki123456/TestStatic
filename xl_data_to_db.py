78259b8f0a9d54173e52623132855ead8d7d5575
ramki123456 <ramki0653@gmail.com>
2017-09-19 18:18:31 +0530

    Update xl_data_to_db.py

--- xl_data_to_db.py
+++ xl_data_to_db.py
@@ -15,3 +15,19 @@ for r in range(0, sheet.nrows):
         LOCATION) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');'''.format(
             data[0].value, data[1].value, data[2].value, data[3].value, data[4].value))
     conn.commit()
+
+    
+    
+    def upload_csv(request):
+	csv_path_name = csvparse()
+	try:
+		csv_file = request.FILES["csv_file"]
+		if not csv_file.name.endswith('.csv'):
+			return HttpResponse("File is not CSV type please upload csv file")
+        #if file is too large, return
+		if csv_file.multiple_chunks():
+			return HttpResponse("Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
+	except Exception as e:
+		print str(e)
+	filepath = os.path.join(csv_path_name+"/", "csv_file")
+	return HttpResponse("File has been uploaded successfully")
